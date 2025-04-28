6ce626bf1004dff34971bd15766a8843dcfe4987
Candela <candelamarzaroli@gmail.com>
2025-04-28 20:36:37 -0300

    repo creado ;P

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import art from "../assets/art.jpeg";
+
+const Home = () => {
+    return (
+        <div>
+        <img src={art} alt="Placeholder Image" />
+        </div>
+    );
+}
+export default Home;
